76da1baf1f8b590682eea2288704c4c2a13db2a3
5CR1P73R43D4N <64167358+speedcubeconference@users.noreply.github.com>
2020-05-14 18:20:41 -0400

    Create info.js

---
+++ info.js
@@ -0,0 +1,2 @@
+console.log("WARNING!")
+console.log("Now obviously this is where I would give you a warning. Obviously I'm not going to because there's no account to steal, no info to get from you, so leave it be! But honestly, don't paste any scipts in here unless you know what they do. If someone says \'This script gives you free money OMG!\' obviously don't believe them. Verify that for yourself.") 
